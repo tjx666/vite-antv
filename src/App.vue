511c3e9c8dc060d4b1e9471d65a28e9e889b6709
长风 <changfeng@gaoding.com>
2021-07-21 18:50:29 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <a-button @click="increment">{{ count }}</a-button>
+</template>
+
+<script lang="ts">
+import { ref, onMounted, defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+  mounted() {
+    const hello = 'hello ';
+    const world = 'world';
+
+    console.log(`${hello}${world}`);
+  },
+});
+</script>
+
+<style></style>
